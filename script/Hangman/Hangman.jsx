9749eaed3f4dcb87d62d417aacea924b61016201
poolpal <zoo5871@naver.com>
2020-08-19 00:48:39 +0900

    2020-08-19

--- script/Hangman/Hangman.jsx
+++ script/Hangman/Hangman.jsx
@@ -1,24 +1,103 @@
-import React,{useState} from 'react';
+import React,{useState,useRef, useEffect} from 'react';
 import txt from './Words.txt'
+import './Hangman.css';
+import Under from './underline';
+import Button from 'react-bootstrap/Button';
 
+const position={
+    1:-4,
+    2:-287,
+    3:-570,
+    4:-833,
+    5:-1138,
+    6:-1421,
+    7:-1704,
+    8:-1987,
+    9:-2270,
+    10:-2553
+}
 
 const Words=(file)=>{
     var word=[];
 
-    word=file.split('\n');
+    word=file.split(';');
 
     return word;
 
 }
+const makemap=(array)=>{
+    const temp=[];
+    var size=array.length;
+
+    for(var i=0;i<size;i++){
+        temp[i]=i;
+    }
+
+    console.log(size);
+
+    return temp;
+}
+
 
 const Hangman=()=>{
     
-    const [GameWords, setGameWords] = useState(Words(txt));
+    //const [GameWords, setGameWords] = useState(Words(txt));  
+    const [imgCoord, setimgCoord] = useState(position[1]);
+    
+    var save; 
+    var temp;
+    //var result=[];   
+    save=Words(txt);
+    temp=save[Math.floor(Math.random()*3)];
+    var empty=[];
 
+    for (var i = 0; i < temp.length; i++) {
+        empty.push('?');
+    }
+  
+    //result=save[Math.floor(Math.random()*2)];
+    
+    const [result, setresult] = useState(empty);
+   
+   
+
+    const onClickBtn=()=>{
+        result[3]="i";
+        setresult((prevresult)=>[...prevresult]);
+
+        console.log(result);
+    }
 
     return(
-        <>
-            {GameWords[0]}
+        <> 
+            <div>
+                 <h2>HangMan</h2>
+            </div>
+            <div id="hang" style={
+                {
+                    backgroundImage:'url(../../img/hangman.png)',
+                    //-4,-287,-570,-833,-1138,-1421,-1704,-1987,-2270,-2553
+                    backgroundPosition:imgCoord,
+                    
+                }
+            }></div>
+            <div>
+                {result}
+            </div>
+
+            <div>
+                {
+                    
+                    result.map((v,i)=>{
+                        return(   
+                            <Under key={v+i} v={v}></Under>
+                        );
+                    })
+                }
+            </div>
+            <div>
+                <Button onClick={onClickBtn}>Check</Button>
+            </div>
         </>
     );
 }
---
+++ script/Hangman/underline.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Under=(props)=>{
+
+    const {v}=props;
+    return(
+    <>
+        <span id="under">
+            {v}
+        </span>
+        
+
+        <span id="space">
+            &nbsp;  
+        </span>
+                                 
+    </>  
+    
+    );
+}
+
+export default Under;
